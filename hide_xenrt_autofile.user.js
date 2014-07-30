9049d72a90b7dae0ea4882c3e48b1379220769ac
John Else <john.else@citrix.com>
2014-07-30 13:24:34 +0100

    Update for Jira 6
    Update to
    
    * unhide all comments before attempting to remove anything
    * fix the removal of autofile comments based on new HTML.
    
    Signed-off-by: John Else <john.else@citrix.com>

--- hide_xenrt_autofile.user.js
+++ hide_xenrt_autofile.user.js
@@ -6,13 +6,25 @@
 // ==/UserScript==
 
 function hide_seen_agains() {
-  var divs = document.getElementsByClassName("issue-data-block")
-  for (var i = 0; i < divs.length; i++) {
-    var contents = divs[i].innerHTML;
-    if (contents.indexOf("XenRT autofile") != -1) {
-      divs[i].innerHTML = "";
-    };
-  };
+  // First unhide all the comments.
+  document.querySelector("a.collapsed-comments").click();
+
+  // Then hide the ones we don't want (after waiting a short while for the
+  // click() operation to happen).
+  setTimeout(
+    function() {
+      var divs = document.querySelectorAll("div.activity-comment");
+      console.log("found comments: " + divs.length);
+      for (var i = 0; i < divs.length; i++) {
+        if (divs[i]
+                .querySelectorAll('p')[0]
+                .innerHTML
+                .lastIndexOf("Seen again on XenRT JobID:") == 0) {
+          divs[i].parentNode.removeChild(divs[i]);
+        };
+      };
+    },
+    3000);
 }
 
 window.addEventListener("load", function(e) {
