0be02f43344653a187aa928f65ee5fd21ee28527
John Else <john.else@gmail.com>
2017-10-25 09:50:09 +0100

    Add hide_redmine_banner.user.js
    Signed-off-by: John Else <john.else@gmail.com>

---
+++ hide_redmine_banner.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name          Hide Redmine maintenance banner
+// @description   Hide the yellow "Redmine will be down..." banner in Redmine
+// @include       https://redmine.*.com/*
+// ==/UserScript==
+
+function hide_banner() {
+  var divs = document.querySelectorAll("div#maintenance_mode_banner");
+
+  for (var i = 0; i < divs.length; i++) {
+    div.style.visibility = 'hidden';
+  }
+}
+
+window.addEventListener("load", function(e) {
+  hide_banner()
+}, false);
